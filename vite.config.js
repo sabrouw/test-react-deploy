6c828f96195e547a2636ae138fe92f016b596a19
sabrouw <sabrow@hotmail.fr>
2024-05-15 18:30:34 +0200

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Spécifie le répertoire de sortie du build
+    sourcemap: true, // Génère des sourcemaps pour le débogage
+    minify: 'esbuild', // Minification des fichiers avec esbuild
+  },
+  server: {
+    port: 3000, // Port du serveur de développement
+    open: true, // Ouvre le navigateur automatiquement
+  },
 })
